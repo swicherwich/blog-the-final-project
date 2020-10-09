61c1370b9879a6c702982a1764b4e644fe908e68
swicherwich <gitgilfoyle@gmail.com>
2020-10-09 22:01:43 +0300

    view refactor

--- src/main/java/io/blog/my/model/User.java
+++ src/main/java/io/blog/my/model/User.java
@@ -34,7 +34,6 @@ public class User extends AbstractEntity {
 			name = "user_role",
 			joinColumns = @JoinColumn(name = "user_id"),
 			inverseJoinColumns = @JoinColumn(name = "role_id")
-			
 	)
 	private Set<Role> roles;
 	
