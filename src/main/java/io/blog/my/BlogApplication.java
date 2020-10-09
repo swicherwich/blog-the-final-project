2328e5344ad2d48717aff3180cc277271ddc9ce0
swicherwich <gitgilfoyle@gmail.com>
2020-10-09 23:24:45 +0300

    added SpringBootApplication class

---
+++ src/main/java/io/blog/my/BlogApplication.java
@@ -0,0 +1,13 @@
+package io.blog.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class BlogApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
