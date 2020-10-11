b9c099aac1e87cb190489898a5d7889a103437b4
swicherwich <gitgilfoyle@gmail.com>
2020-10-11 17:07:21 +0300

    added BCryptPasswordEncoder @Bean

--- src/main/java/io/blog/my/BlogApplication.java
+++ src/main/java/io/blog/my/BlogApplication.java
@@ -2,12 +2,18 @@ package io.blog.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.swing.*;
 
 @SpringBootApplication
 public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
