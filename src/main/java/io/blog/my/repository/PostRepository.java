2ce670c14459031ee5db88312d6b2a860b86d805
swicherwich <gitgilfoyle@gmail.com>
2020-10-14 11:13:08 +0300

    added Post repository

---
+++ src/main/java/io/blog/my/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package io.blog.my.repository;
+
+import io.blog.my.model.Post;
+import io.blog.my.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+	Page<Post> findByUserOrderByCreateDateDesc(User user, Pageable pageable);
+	Page<Post> findAllByOrderByCreateDateDesc(Pageable pageable);
+	Optional<Post> findById(Long id);
+}
