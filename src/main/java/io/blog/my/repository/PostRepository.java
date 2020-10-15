f60caf6ee8874e11f265416a1dd5faa2b03587ba
swicherwich <gitgilfoyle@gmail.com>
2020-10-15 15:49:10 +0300

    edited ropositories

--- src/main/java/io/blog/my/repository/PostRepository.java
+++ src/main/java/io/blog/my/repository/PostRepository.java
@@ -5,9 +5,11 @@ import io.blog.my.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 	Page<Post> findByUserOrderByCreateDateDesc(User user, Pageable pageable);
 	Page<Post> findAllByOrderByCreateDateDesc(Pageable pageable);
--- src/main/resources/db/migration/V2__new_user_column_added.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-ADD column enabled bool not null default false;
