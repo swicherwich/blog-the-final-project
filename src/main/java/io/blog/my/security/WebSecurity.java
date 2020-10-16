8d73fc1222ab81e57acb1938ad3e1df949b35ae4
swicherwich <gitgilfoyle@gmail.com>
2020-10-16 20:37:54 +0300

    removed unnecessary method, edited configure method

--- src/main/java/io/blog/my/security/WebSecurity.java
+++ src/main/java/io/blog/my/security/WebSecurity.java
@@ -1,49 +1,61 @@
 package io.blog.my.security;
 
-import io.blog.my.service.UserService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEF_USERS_BY_USERNAME_QUERY;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
-	private final UserService userService;
+	private final AccessDeniedHandler accessDeniedHandler;
+	private final DataSource dataSource;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.userService = userService;
+	public WebSecurity(AccessDeniedHandler accessDeniedHandler,
+	                   DataSource dataSource,
+	                   BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.accessDeniedHandler = accessDeniedHandler;
+		this.dataSource = dataSource;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/sign-up/**", "/sign-in/**")
-				.permitAll()
-				.anyRequest()
-				.authenticated()
+		http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/home", "/sign-up/**", "/error", "/blog/**", "/post/**").permitAll()
+				.antMatchers("/newPost/**", "/commentPost/**", "/createComment/**").hasAnyRole("USER")
+				.anyRequest().authenticated()
 				.and()
 				.formLogin()
 				.loginPage("/sign-in")
-				.permitAll();
+				.defaultSuccessUrl("/home")
+				.permitAll()
+				.and()
+				.logout()
+				.permitAll()
+				.and()
+				.exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+				.and().headers().frameOptions().disable();
 	}
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+		final String rolesQuery = "select u.username, r.role from users as u " +
+									"inner join user_role as ur on(u.id = ur.user_id) " +
+									"inner join roles as r on (ur.role_id = r.id) where u.username = ?";
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery(DEF_USERS_BY_USERNAME_QUERY)
+			.authoritiesByUsernameQuery(rolesQuery)
+			.dataSource(dataSource)
+			.passwordEncoder(bCryptPasswordEncoder);
 	}
-	
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-		return source;
-	}
+
 }
