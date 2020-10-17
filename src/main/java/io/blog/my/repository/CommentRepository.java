4d2db436d081dec6367eace5e65a5a569d3e1095
swicherwich <gitgilfoyle@gmail.com>
2020-10-17 13:40:26 +0300

    added Comment repositiory

---
+++ src/main/java/io/blog/my/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package io.blog.my.repository;
+
+import io.blog.my.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
